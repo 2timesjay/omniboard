d6c4f2962b79cea8665707cea891180422d375d1
unknown <2timesjay@gmail.com>
2021-10-25 10:35:53 -0400

    More type declaraions, especially input acqusition

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
 interface Selectable {};
 
-type Stack = (Selectable | Array<Selectable>);
+type ContextStack = (Selectable | Array<Selectable>);
 
-type Increment = (Stack) => Array<Selectable>;
+// Hole here: Increment has no state, so a bfs must 
+// specify a fixed depth or globally control stopping.
+type Increment = (ContextStack) => Array<Selectable>;
 
 type Options = Array<Selectable>;
 
 declare function getOptions(sel: Selectable): Options;
 
-interface Location {};
-
-type Neighborhood = Array<Location>;
-
-declare function getNeighborhood(loc: Location): Neighborhood;
-
-// For efficient implementation, PreviewMap should _look_ like a Selectable: Stack map,
+// For efficient implementation, PreviewMap should _look_ like a Selectable: ContextStack map,
 // but in fact the set of stacks should be stored as a single Tree.
 // https://www.typescriptlang.org/docs/handbook/2/mapped-types.html doesn't work
 // because it requires a string or number key. So actually Use Maps
 // https://stackoverflow.com/questions/30019542/es6-map-in-typescript/30112075#30112075
-type PreviewMap = Map<Selectable, Stack>;
+type PreviewMap = Map<Selectable, ContextStack>;
 
-declare function getOptionPreviewMap(stack: Stack, incr: Increment): PreviewMap;
+declare function getOptionPreviewMap(stack: ContextStack, incr: Increment): PreviewMap;
 
-declare function bfs(stack: Stack, incr: Increment): Options;
+declare function bfs(stack: ContextStack, incr: Increment): Options;
+
+declare function bfsPreviewable(stack: ContextStack, incr: Increment): PreviewMap;
 
 // State Stuff
 
@@ -33,13 +31,30 @@ interface State {};
 // https://www.typescriptlang.org/docs/handbook/2/functions.html#call-signatures
 type Effect = {
     description: string;
+    pre_effect: Observer;
+    post_effect: Observer;
     (state: State): State;
 };
 
 type Observer = {
     description: string;
-    (effect: Effect): Array<Effect>;
+    (effect: Effect, state: State): Array<Effect>;
 };
+
+type EffectStack = Array<Effect>; // Replace Array with a custom Stack datastructure?
+
+// Input Stuff
+
+type OptionsGenerator = (args: any[]) => Options; // ...args?
+
+type PreviewableOptionsGenerator = (args: any[]) => PreviewMap; // ...args?
+
+interface InputAcquisition {
+    getAcquisitionSequence: (stack: ContextStack) => Array<OptionsGenerator>;
+    acquireInputs: (acquisition_sequence: Array<OptionsGenerator>) => ContextStack;
+};
+
+type EffectGenerator = (ContextStack) => Effect
 
 // Time Stuff: Likely to be refactored.
 
@@ -48,3 +63,25 @@ interface Phase {};
 interface TimePoint {};
 
 type Timeline = Array<[TimePoint, Phase]>;
+
+// Space Stuff
+
+interface Location {};
+
+type Neighborhood = Array<Location>;
+
+declare function getNeighborhood(loc: Location): Neighborhood;
+
+// Card Stuff
+
+interface Card {};
+
+interface Hand {
+    cards: Array<Card>;
+}
+
+interface Deck {
+    cards: Array<Card>;
+    draw: () => Card; // Draw
+    shuffle: () => void; // Shuffle
+};
