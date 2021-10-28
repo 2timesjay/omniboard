4dc8eeb0163db31675f7997537c7b11c2edd3875
unknown <2timesjay@gmail.com>
2021-10-27 22:02:27 -0400

    sample tape test

---
+++ src/model/space.ts
@@ -0,0 +1,53 @@
+// types = require("./types.ts");
+// import {Neighborhood} from "./types"
+
+enum RelativeCoordinateOperator {
+    BASE,
+    REPEAT,
+    CONCAT,
+    OR,
+}
+
+class RelativeCoordinate {
+    root: RelativeCoordinateOperator;
+    value: number[];
+    children: Array<RelativeCoordinate>;
+};
+
+type RelativeNeighborhood = Array<RelativeCoordinate>
+
+interface ILocation {}
+
+type Neighborhood = Array<ILocation>;
+
+interface IDiscreteLocation extends ILocation {}
+
+class GridLocation implements IDiscreteLocation {
+    x: number;
+    y: number;
+    z: number;
+
+    dim: number;
+
+    constructor(x: number, y: number, z?: number){
+        this.x = x;
+        this.y = y;
+        if (z) {
+            this.z = z;
+            this.dim = 3;
+        }
+        else { 
+            this.dim = 2;
+        }
+    };
+}
+
+class GridSpace {
+    locs: GridLocation[][] // | GridLocation[][][] // use ndarrays? https://github.com/scijs/ndarray
+
+
+
+    getNeighborhood(rel_ne: RelativeNeighborhood, loc: GridLocation): Neighborhood {
+        return Array<GridLocation>();
+    }
+}
