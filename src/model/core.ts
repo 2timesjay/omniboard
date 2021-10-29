ed92c57d051c049d8e931d391dcf2ebf3cf41b26
unknown <2timesjay@gmail.com>
2021-10-28 21:08:44 -0400

    flesh out Tree, add stack tests

--- src/model/core.ts
+++ src/model/core.ts
@@ -1,12 +1,23 @@
-export interface T {}
+import { IScriptSnapshot } from "typescript";
+
+export interface ISelectable {}
+
+type Options = Array<ISelectable>;
 
 export class Stack<T> {
     value: T;
     parent: Stack<T> | null;
+    depth: number;
     
-    constructor(value: T, parent: Stack<T>) {
+    constructor(value: T, parent?: Stack<T>) {
         this.value = value;
-        this.parent = parent;
+        if (parent) {
+            this.parent = parent;
+            this.depth = parent.depth + 1;
+        } else {
+            this.parent = null;
+            this.depth = 1;
+        }
     }
 
     push(sel: T): Stack<T> {
@@ -35,36 +46,77 @@ export class Stack<T> {
     }
 }
 
-// export class Deque<T> {
-//     value: T;
-//     middle: Deque<T>;
-//     bottom: T;
-// }
-
-export class Tree<T> {
+export class Tree<T> extends Stack<T> {
     value: T;
     parent: Tree<T> | null;
     children: Array<Tree<T>> | null;
+    depth: number;
 
     constructor(value: T, parent?: Tree<T>, children?: Array<Tree<T>>){
-        this.value = value;
-        if (parent) {
-            this.parent = parent;
-        } else {
-            this.parent = null;
-        }
-
+        super(value, parent);
         if (children) {
             this.children = children;
         } else {
             this.children = null;
         }
     }
+
+    add_child(child: T): Tree<T> {
+        var child_tree = new Tree<T>(child, this);
+        if (this.children) {
+            this.children.push(child_tree);
+        }
+        else {
+            this.children = new Array<Tree<T>>(child_tree);
+        }
+        return child_tree;
+    }
+
+    to_map(): Map<T, Tree<T>> {
+        if (this.children) {
+            var map = new Map<T, Tree<T>>()
+            map.set(this.value, this);
+            this.children.forEach(
+                (child) => {
+                    var child_map = child.to_map();
+                    child_map.forEach((value, key) => map.set(key, value));
+                }
+            )
+        } else {
+            var map = new Map<T, Tree<T>>()
+            map.set(this.value, this);
+        }
+        return map;
+    }
 }
 
-export class PreviewMap<T> {
-    // A Map<T, Stack<T>>
-    constructor(tree: Tree<T>) {
+type IncrementFn = (stack: Stack<ISelectable>) => Options;
 
+type TerminationFn = (stack: Stack<ISelectable>) => boolean;
+
+export function bfs(
+    root: ISelectable, 
+    increment_fn: IncrementFn, 
+    termination_fn: TerminationFn
+): Tree<ISelectable> {
+    var preview_map = new Tree<ISelectable>(root); 
+    var explored = new Set<ISelectable>();
+    var to_explore = new Array<Tree<ISelectable>>(preview_map);
+    while (to_explore.length > 0) {
+        var exploring = to_explore.shift();
+        if (!termination_fn(exploring)){
+            var candidates = increment_fn(exploring);
+            var filtered_candidates = candidates.filter(
+                (candidate) => !(explored.has(candidate))
+            );
+            filtered_candidates.forEach(
+                (candidate) => {
+                    var candidate_tree = exploring.add_child(candidate);
+                    to_explore.push(candidate_tree);
+                }
+            )
+        }
+        explored.add(exploring.value);
     }
+    return preview_map;
 }
