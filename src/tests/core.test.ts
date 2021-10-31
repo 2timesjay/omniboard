34fee81d66eb4a7ea462f31eb3c7aa81f3d18ecf
unknown <2timesjay@gmail.com>
2021-10-30 20:15:08 -0400

    test BFS. Have to cache outside bfs function

--- src/tests/core.test.ts
+++ src/tests/core.test.ts
@@ -1,5 +1,13 @@
 import * as test from "tape";
-import {Stack, Tree} from "../model/core";
+import {bfs, ISelectable, Stack, Tree} from "../model/core";
+
+class SelectableNumber implements ISelectable {
+    value: number;
+
+    constructor(value: number) {
+        this.value = value;
+    }
+}
 
 test("Stack Test", (t) => {
     var stack = new Stack<number>(1);
@@ -19,4 +27,29 @@ test("Stack Test", (t) => {
 test("Tree Test", (t) => {
     var tree = new Tree<number>(1);
     t.end()
+})
+
+test("BFS test", (t) => {
+    var numbers = new Array<SelectableNumber>();
+    for (var i = 0; i < 100; i++) {
+        numbers.push(new SelectableNumber(i));
+    }
+    var root_stack = new Stack<SelectableNumber>(new SelectableNumber(3));
+    var increment_fn = (nums: Stack<SelectableNumber>): Array<SelectableNumber> => {
+        var options = Array<SelectableNumber>();
+        var current_num = nums.value.value;
+        options.push(numbers[current_num+3]);
+        options.push(numbers[current_num+6]);
+        return options;
+    };
+    var termination_fn = (nums: Stack<SelectableNumber>): boolean => {
+        return nums.depth > 3;
+    }
+    var tree = bfs(root_stack, increment_fn, termination_fn);
+    var map = tree.to_map();
+    console.log(map);
+    // fails because no customizable object equality in Map/Set.
+    // Or works if caching moved outside bfs layer (which stinks)
+    t.equal(map.size, 7); 
+    t.end();
 })
