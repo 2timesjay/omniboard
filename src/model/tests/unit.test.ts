814f7f1ad2e4b20bec168f45ba65651df483869c
unknown <2timesjay@gmail.com>
2022-05-28 11:31:58 -0400

    add unit unit-tests

---
+++ src/model/tests/unit.test.ts
@@ -0,0 +1,12 @@
+import * as test from "tape";
+import { BoardState } from "../state";
+import {Unit, CONSTRUCT_BASIC_ACTIONS} from "../unit";
+
+test("Unit test", (t) => {
+    var unit = new Unit(0);
+    t.equals(unit.team, 0);
+    var state = new BoardState();
+    unit.actions = CONSTRUCT_BASIC_ACTIONS(unit, state);
+    t.equals(unit.actions.length, 4);
+    t.end();
+})
