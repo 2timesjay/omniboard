2f048cd4ac4b3fb2f0cf4292b2873c3a05c3e67e
unknown <2timesjay@gmail.com>
2022-06-11 00:52:43 -0400

    status and observer stubs

---
+++ src/model/status.ts
@@ -0,0 +1,15 @@
+import { DigestFn } from "./action";
+import { ISelectable } from "./core";
+import { Effect } from "./effect";
+
+type TriggerCond = (e: Effect) => boolean;
+
+class Status {
+    trigger_condition: TriggerCond;
+    digest_fn: DigestFn<ISelectable>;
+
+    constructor(trigger_condition: TriggerCond, digest_fn: DigestFn<ISelectable>) {
+        this.trigger_condition = trigger_condition;
+        this.digest_fn = digest_fn;
+    }   
+}
